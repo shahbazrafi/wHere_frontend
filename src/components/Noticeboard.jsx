2972a56ba68f94e76f430c16b8869072ee5cf9a8
sasjaWijdenes <saswij88@gmail.com>
2022-11-24 11:05:23 +0000

    adds further animation to noticeboard and cards

--- src/components/Noticeboard.jsx
+++ src/components/Noticeboard.jsx
@@ -12,7 +12,7 @@ const Noticeboard = ({ events, setEvents }) => {
             setIsExpanded(state => !state)
         }
     let key = 0;
-    return <motion.div transition={{layout: {duration: 1.2, type: 'spring' }}} layout className="noticeboard" onClick={toggleExpand} whileHover={{ scale: 1.05 }}>
+    return <motion.div transition={{layout: {duration: 1.2, type: 'spring' }}} layout initial={{opacity: 0, translateY: -50}} animate={{opacity: 1, translateY: 0}} className="noticeboard" onClick={toggleExpand} whileHover={{ scale: 1.05 }}>
         {!isExpanded ? <motion.li whileHover={{ scale: 1.01 }} layout='transition' className="event-item"><Event event={events[events.length-1]} /></motion.li>: <></>}
         {isExpanded && <motion.ul  layout className="event-list">
             {events.map((event, index) => {
